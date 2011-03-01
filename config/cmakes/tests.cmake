e4c1c4af6c728e2f524eeb8cdf2b36b430caa67b
Kevin TRAN <vexin.m@gmail.com>
2011-03-01 09:10:41 -0800

    Added license

--- config/cmakes/tests.cmake
+++ config/cmakes/tests.cmake
@@ -1,9 +1,11 @@
 set(WORKING_DITECTORY ${bin_dir})
 
 enable_testing()
-
+include(Dart)
 include(CTest)
 
 #TODO: check create_test_sourcelist
 #add_executable()
+#set_target_property( PROPERTIES RUNTIME_OUTPUT_LIBRARY )
 #add_test()
+
